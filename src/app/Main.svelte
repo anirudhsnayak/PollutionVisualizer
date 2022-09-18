bf6d73b93519fa78d4b34a3e4f68d96042ada66a
anirudhsnayak <anirudhsnayak@gmail.com>
2022-09-17 23:04:06 -0700

    Initial commit

---
+++ src/app/Main.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div class="body">
+    Hello, World!
+</div>
+
+<style type="text/scss">
+    .body {
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        min-height: 100%;
+        width: 100%;
+        top: 0; left: 0;
+    }
+</style>
+
